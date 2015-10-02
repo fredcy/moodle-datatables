2e2fd6ef9a6072497b04f15573ae85b2288b10ae
Fred Yankowski <fcy@imsa.edu>
2015-10-02 12:50:57 -0500

    Update version.php.

--- version.php
+++ version.php
@@ -1,5 +1,7 @@
 <?php
 
-$plugin->version  = 2015093000;
+$plugin->version  = 2015100200;
 $plugin->requires = 2015050500;
 $plugin->component = 'tool_datatables';
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = 'v0.1 for Moodle 2.9+';
