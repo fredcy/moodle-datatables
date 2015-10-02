f36116ed4188d85497ffdef995ec62ecc9ba6e42
Fred Yankowski <fcy@imsa.edu>
2015-10-02 11:02:58 -0500

    Change init() to take CSS selector as new first argument.

--- amd/src/init.js
+++ amd/src/init.js
@@ -1,26 +1,31 @@
-define(['jquery', 'tool_datatables/jquery.dataTables', 'tool_datatables/dataTables.bootstrap',
-	'tool_datatables/dataTables.select', 'tool_datatables/dataTables.buttons',
-	'tool_datatables/buttons.bootstrap'],
+define(['jquery', 'tool_datatables/jquery.dataTables', 'core/log',
+	'tool_datatables/dataTables.bootstrap', 'tool_datatables/dataTables.select',
+	'tool_datatables/dataTables.buttons', 'tool_datatables/buttons.bootstrap'],
        function ($, datatables) {
 	   return {
 	       test: function() {
 		   window.console.log('$.fn is:'); window.console.log($.fn);
 		   window.console.log('datatables is:'); window.console.log(datatables);
 	       },
-	       init: function(params) {
+	       init: function(selector, params) {
+		   if (params.debug) {
+		       window.console.log('tool_datatables:init.js/init(): ', selector, params);
+		   }
 		   var options = {
 		       'autoWidth': false,
 		       'paginate': false,
 		       'order': [],		// disable initial sort
+		       'buttons': [],
+		       'dom': 'Bftrip',		// position buttons
 		   };
 		   $.extend(true, options, params); // deep-merge params into options
 		   for (var attrname in params) {
 		       options[attrname] = params[attrname];
 		   }
-		   if (options.debug) {
-		       window.console.log('options = ', options);
+		   if (params.debug) {
+		       window.console.log('tool_datatables init.js/init(): options = ', options);
 		   }
-		   $('.datatable').DataTable(options);
+		   $(selector).DataTable(options);
 	       },
 	   };
        }
